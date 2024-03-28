801d1650dcd5d605b88568f4abb579e5e2b7a499
isaacyan3687 <isaacyan3687@gmail.com>
2024-03-28 16:52:25 +1100

    basic nav-bar

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+// src/pages/index.tsx
+import React from 'react';
+import Navbar from '@/components/Navbar';
+
+const HomePage= () => (
+  <div>
+    <Navbar />
+    <p>Welcome to my personal website!</p>
+  </div>
+);
+
+export default HomePage;
